348b3ce3bddeef128459d82ab42226104bd401c4
Spl3en <spl3en.contact@gmail.com>
2019-04-17 20:16:45 +0200

    Add some filters

--- sql/scripts/show_transaction_message_data.sql
+++ sql/scripts/show_transaction_message_data.sql
@@ -10,4 +10,7 @@ WHERE
     and CAST(UNHEX(SUBSTR(data, 3)) AS CHAR(500000)) != "icxgames reward payout"
     and CAST(UNHEX(SUBSTR(data, 3)) AS CHAR(500000)) != "icxgames lotto divide bet"
     and CAST(UNHEX(SUBSTR(data, 3)) AS CHAR(500000)) != "icxgames lotto return"
+    and CAST(UNHEX(SUBSTR(data, 3)) AS CHAR(500000)) NOT LIKE "1USD : %"
+    and CAST(UNHEX(SUBSTR(data, 3)) AS CHAR(500000)) NOT LIKE "[Mainnet]%"
+    and CAST(UNHEX(SUBSTR(data, 3)) AS CHAR(500000)) NOT LIKE "[UPbit]%"
 ORDER BY transaction DESC
